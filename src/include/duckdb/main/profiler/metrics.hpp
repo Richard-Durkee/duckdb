7f88e202f5c507b7d09501a6b538979ecdec4cfa
Richard Durkee <Richard-Durkee@users.noreply.github.com>
2026-09-11 15:18:52 -0400

    Add Reported Per-Operator Peak Memory Profiling Metric

--- src/include/duckdb/main/profiler/metrics.hpp
+++ src/include/duckdb/main/profiler/metrics.hpp
@@ -226,6 +226,13 @@ struct MetricOperatorIntermediateSizeBytes {
 	static constexpr const char *Unit = "bytes";
 	static constexpr const char *TypeStr = "uint64";
 };
+struct MetricOperatorPeakMemory {
+	using METRIC_TYPE = uint64_t;
+	static constexpr const char *Name = "operator.peak_memory";
+	static constexpr const char *Description = "Peak memory held by the operator, as reported by the operator itself";
+	static constexpr const char *Unit = "bytes";
+	static constexpr const char *TypeStr = "uint64";
+};
 struct MetricOperatorRowGroupsScanned {
 	using METRIC_TYPE = uint64_t;
 	static constexpr const char *Name = "operator.row_groups_scanned";
